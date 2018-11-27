73510c3ee4a92a63ce9b33221c54e01ab00001ba
Nico Domino <ndomino@newtelco.de>
2018-11-27 18:05:50 +0100

    working chrome add-er extensiongit add maintenanceExtension/*

--- maintenanceExtension/content.js
+++ maintenanceExtension/content.js
@@ -1,47 +1,39 @@
 'use strict';
 
-/*************************** 
+/***************************
  * https://www.inboxsdk.com/docs/#ToolbarButtonDescriptor
- * 
+ *
  * InboxSDK_ID: sdk_newtelco_1_c1fb774414
  *
  */
 
 InboxSDK.load('1', 'sdk_newtelco_1_c1fb774414').then(function(sdk){
 
-    /*
-    sdk.Toolbars.registerThreadRowViewHandler(function(threadView1){
-        threadView1.addButton({
-            type: "LINK",
-            title: "Add to Maint",
-            iconUrl: 'https://newtelco.tech/done2.png',
-            onClick:  function(event) {
-                event.getMessageID().then(msgId => msgId.toLowerCase());
-                event.getThreadID().then(threadId => threadId.toLowerCase());
-                openInNewTab('https://maintenance.newtelco.de/addedit?gmid=123') ;
-            }
-        });
+    sdk.Lists.registerThreadRowViewHandler(function(threadView1){
+      var threadClicked = threadView1.getThreadIDAsync();
+      console.log(threadClicked);
+
+      threadView1.addButton({
+        title: "Send to Maintenance2",
+        iconUrl: chrome.extension.getURL('images/nt48.png'),
+        onClick: function(event) {
+          threadView1.addLabel({
+            title: "Sent to Maintenance",
+            iconUrl: chrome.extension.getURL('images/nt48.png'),
+            backgroundColor: "#67B246",
+            foregroundColor: "#fff"
+          })
+          console.log("threadView1 onClick: " + threadClicked);
+          threadClicked.then(function(result) {
+            openInNewTab('https://maintenance.newtelco.de/addedit?gmid=' + result);
+            console.log("threadView1 2: " + result);
+          })
+
+        },
+      });
 
     });
-    sdk.Conversations.registerThreadViewHandler
-   */
 
-    sdk.Toolbars.registerThreadRowViewHandler(function(threadView1){
-        
-    });
-	sdk.Toolbars.registerThreadButton({
-			title: "Send to Maintenance",
-            iconUrl: 'https://newtelco.tech/done2.png',
-            positions: ['THREAD','ROW'],
-            onClick:  function(event) {
-                var id = threadView1.getThreadIDAsync(event);
-                console.log("gmid: " + id);
-                openInNewTab('https://maintenance.newtelco.de/addedit?gmid');
-            }
-    });
-    
-                
-    
     function openInNewTab(url) {
         var win = window.open(url, '_blank');
         // win.focus();

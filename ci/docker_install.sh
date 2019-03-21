7e9ce3d98fd963ee1259b873d30c1d1fa4f38547
Nico Domino <ndomino@newtelco.de>
2019-03-21 16:45:24 +0100

    updated gitlab-ci.yml

--- ci/docker_install.sh
+++ ci/docker_install.sh
@@ -23,5 +23,4 @@ chmod +x /usr/local/bin/phploc
 
 # Install mysql driver
 # Here you can install any other extension that you need
-docker-php-ext-install mysqli gd zip
-
+docker-php-ext-install mysqli gd zip xml gmp

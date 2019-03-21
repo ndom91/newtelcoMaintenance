95c9460b5b91e8a070c91aacb8b39b34deb06fc9
Nico Domino <ndomino@newtelco.de>
2019-03-21 17:19:57 +0100

    updated gitlab-ci.yml

--- ci/docker_install.sh
+++ ci/docker_install.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# composer + npm deploy:
+#  https://docs.gitlab.com/ee/ci/examples/deployment/composer-npm-deploy.html
+# 
 # We need to install dependencies only for Docker
 [[ ! -e /.dockerenv ]] && exit 0
 
@@ -15,12 +18,27 @@ apt-get install git zip unzip libpng-dev libgmp-dev -yqq
 #curl --location --output /usr/local/bin/phpunit https://phar.phpunit.de/phpunit.phar
 #chmod +x /usr/local/bin/phpunit
 
+# https://github.com/phpmd/phpmd
 curl --location --output /usr/local/bin/phpmd http://static.phpmd.org/php/latest/phpmd.phar
 chmod +x /usr/local/bin/phpmd
 
+# https://github.com/sebastianbergmann/phploc
 curl --location --output /usr/local/bin/phploc https://phar.phpunit.de/phploc.phar
 chmod +x /usr/local/bin/phploc
+
+# Rollbar Deploy Notify
+ACCESS_TOKEN=e0dbc12159df4dd3a6087170e3a8ace0
+ENVIRONMENT=production
+LOCAL_USERNAME=`whoami`
+REVISION=`git rev-parse --verify HEAD`
+curl https://api.rollbar.com/api/1/deploy/ \
+  -F access_token=$ACCESS_TOKEN \
+  -F environment=$ENVIRONMENT \
+  -F revision=$REVISION \
+  -F local_username=$LOCAL_USERNAME
 
 # Install mysql driver
 # Here you can install any other extension that you need
 docker-php-ext-install mysqli gd zip gmp
+
+sed -i 's,^memory_limit =.*$,memory_limit = -1,' php.ini

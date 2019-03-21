4aade8ad1d9d263fed35bca8453af48a66cc42d9
Nico Domino <ndomino@newtelco.de>
2019-03-21 14:33:49 +0100

    updated apt-get dependencies

--- ci/docker_install.sh
+++ ci/docker_install.sh
@@ -7,7 +7,8 @@ set -xe
 
 # Install git (the php image doesn't have it) which is required by composer
 apt-get update -yqq
-apt-get install git phploc phpcpd phpmd php-pear -yqq
+#apt-get install git phploc phpcpd phpmd php-pear -yqq
+apt-get install git php-phpunit-phploc php-phpunit-phpcpd php-phpmd-PHP-PMD -yqq
 
 # Install phpunit, the tool that we will use for testing
 curl --location --output /usr/local/bin/phpunit https://phar.phpunit.de/phpunit.phar
@@ -15,5 +16,5 @@ chmod +x /usr/local/bin/phpunit
 
 # Install mysql driver
 # Here you can install any other extension that you need
-docker-php-ext-install pdo_mysql
+docker-php-ext-install mysqli
 
